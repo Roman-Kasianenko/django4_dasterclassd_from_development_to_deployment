1a7fc26a77858acf0fd9274f2ac888be7b2995de
Roman <kasyan.ua@gmail.com>
2023-08-21 23:04:22 +0300

    section 9

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from .models import Product
@@ -20,3 +20,42 @@ def products(request):
 def product_details(request, id):
     product = Product.objects.get(id=id)
     return render(request, "myapp/detail.html", context={'product': product})
+
+
+def add_product(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        price = request.POST.get('price')
+        desc = request.POST.get('desc')
+        image = request.FILES['upload']
+
+        product = Product(name=name, price=price, desc=desc, image=image)
+        product.save()
+    return render(request, "myapp/addproduct.html")
+
+
+def update_product(request, id):
+    product = Product.objects.get(id=id)
+    context = {'product': product}
+    if request.method == 'POST':
+        product.name = request.POST.get('name')
+        product.price = request.POST.get('price')
+        product.desc = request.POST.get('desc')
+        img = request.FILES.get('upload')
+        if img:
+            product.image = img
+
+        product.save()
+
+        return redirect(to="/myapp/products")
+    return render(request, "myapp/update_product.html", context)
+
+
+def delete_product(request, id):
+    product = Product.objects.get(id=id)
+    context = {'product': product}
+    if request.method == 'POST':
+        product.delete()
+        return redirect(to="/myapp/products")
+
+    return render(request, 'myapp/delete.html', context)
